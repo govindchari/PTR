b8fc7439b79bd6b82ee5486c8c720c0eb678ca16
govindchari <govindchari1@gmail.com>
2022-12-16 13:30:38 -0800

    Refactored for speed

--- src/discretize.jl
+++ src/discretize.jl
@@ -2,7 +2,6 @@ function FOH_discretize(p::ptr)
 
     for k = 1:p.K-1
         idx = p.idx
-        println(k)
 
         # Get integrator initial conditions
         P0 = zeros(idx.N)
@@ -51,39 +50,37 @@ function uprop(τ::Float64, p::ptr)
         return lm * p.uref[:, k] + lp * p.uref[:, k+1]
     end
 end
+function getDTMatrices(τ::Float64, p::ptr)
+    stateProp = getState(τ, uprop, p)
+    A = p.σref * p.dfx(stateProp, uprop(τ, p))
+    B = p.σref * p.dfu(stateProp, uprop(τ, p))
+    S = p.f(stateProp, uprop(τ, p))
+    z = -reshape(A, (p.nx, p.nx)) * stateProp - reshape(B, (p.nx, p.nu)) * uprop(τ, p)
+    return A, B, S, z
+end
 function df(τ::Float64, P::Array{Float64,1}, p::ptr)
 
     idx = p.idx
     k = Int(floor(τ / p.dτ)) + 1
     # Linearized Dynamics
-    lm(t) = (k * p.dτ - t) / p.dτ
-    lp(t) = (t - (k - 1) * p.dτ) / p.dτ
-    # uprop(t) = lm(t) * p.uref[:, k] + lp(t) * p.uref[:, k+1]
+    lm = (k * p.dτ - τ) / p.dτ
+    lp = (τ - (k - 1) * p.dτ) / p.dτ
 
-    hfidx(t) = Int(t / (p.dτ / p.Nsub)) + 1
-    A(t) = p.σref * p.dfx(getState(t, uprop, p), uprop(t, p))
-    B(t) = p.σref * p.dfu(getState(t, uprop, p), uprop(t, p))
-    S(t) = p.f(getState(t, uprop, p), uprop(t, p))
-    z(t) = -reshape(A(t), (p.nx, p.nx)) * getState(t, uprop, p) - reshape(B(t), (p.nx, p.nu)) * uprop(t, p)
-
+    A, B, S, z = getDTMatrices(τ, p)
 
     Px = P[idx.x]
     Pphi = P[idx.phi]
-    PBm = P[idx.Bm]
-    PBp = P[idx.Bp]
-    PS = P[idx.S]
-    Pz = P[idx.z]
 
     Pphi_mat = reshape(Pphi, (p.nx, p.nx))
     F = factorize(Pphi_mat)
 
     dP = zeros(idx.N)
     dP[idx.x] = p.σref * p.f(Px, uprop(τ, p))
-    dP[idx.phi] = reshape(A(τ) * Pphi_mat, (p.nx^2, 1))
-    dP[idx.Bm] = reshape(F \ (lm(τ) * B(τ)), (p.nx * p.nu, 1))
-    dP[idx.Bp] = reshape(F \ (lp(τ) * B(τ)), (p.nx * p.nu, 1))
-    dP[idx.S] = F \ S(τ)
-    dP[idx.z] = F \ z(τ)
+    dP[idx.phi] = reshape(A * Pphi_mat, (p.nx^2, 1))
+    dP[idx.Bm] = reshape(F \ (lm * B), (p.nx * p.nu, 1))
+    dP[idx.Bp] = reshape(F \ (lp * B), (p.nx * p.nu, 1))
+    dP[idx.S] = F \ S
+    dP[idx.z] = F \ z
 
     return dP
 
@@ -92,14 +89,12 @@ function RK4(dz::Function, z0::Array{Float64,1}, t0::Float64, dt::Float64, Nsub:
     h = dt / Nsub
     z = copy(z0)
     for i = 1:Nsub
-        # hifi_idx = Int(t0 / dt) + 1 + i
         τ = t0 + (i - 1) * h
         k1 = dz(τ, z, p)
         k2 = dz(τ + 0.5 * h, z + 0.5 * h * k1, p)
         k3 = dz(τ + 0.5 * h, z + 0.5 * h * k2, p)
         k4 = dz(τ + h, z + h * k3, p)
         z += (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
-        # p.xref_hifi[:, hifi_idx] = z[p.idx.x]
     end
     return z
 end
